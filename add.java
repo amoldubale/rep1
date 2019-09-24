b258fa4298d7d897044e3115553c9d21948d6941
amoldubale <53308775+amoldubale@users.noreply.github.com>
2019-09-24 12:57:19 +0530

    semicolon missing

--- add.java
+++ add.java
@@ -3,7 +3,7 @@ class Amol
 {
 	public static void main(String[] args)
 	{
-		System.out.println("this program is made by THe great Amol DUBAle")
+		System.out.println("this program is made by THe great Amol DUBAle");
 	    for(int i=1;i<=5;i++)
 		{
 			for(int j=1;j<=i;j++)
